395827af6daef53ee6dbe94988e819a2d590b1e6
Alex <lyakishev_i_vah@mail.ru>
2025-09-18 03:38:52 +0300

    crazying

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import manageSystem.ManageAutomaticSystem;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -32,7 +34,14 @@ public class Main {
 //            }
 //            System.out.println("END Print current massive");
            // System.out.println("PPP "+ comandsString[0]);
-            ArrayList<String> comandsString_BUFFER = new ArrayList<String>(comandsString.length);
+
+            ArrayList<String> comandsString_BUFFER = new ArrayList<String>();
+            for(int i = 0; i < comandsString.length; i++){
+                comandsString_BUFFER.add(comandsString[i]);
+            }
+
+            System.out.println("SSSSSSSSSSSSSSSSIIIIZEEEEEEEEEEEE " +comandsString.length+" "+ comandsString_BUFFER.size());
+            //System.out.println("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD " +Arrays.asList(comandsString));
             String buf_str;
             boolean thereAreAttachments = false;
             for(int i = 0; i < comandsString.length; i++){
@@ -41,19 +50,19 @@ public class Main {
                 //comandsList.add("ioioioioi "+i);
                 if (comandsString[i].indexOf("controls_ifelse") != -1){
                     int u = 0;
-                    comandsList.add("IF");
+                    comandsString_BUFFER.add(i,  "IF");
                     while (comandsString[i + u].indexOf("next") == -1){
                         buf_str += comandsString[i + u];
                         u += 1;
                     }
                     buf_str += comandsString[i + u];
                     i += u;
-                    comandsList.add(  buf_str.split("IF0")[1].split(",\"DO0\":")[0] );
-                    comandsList.add("DO");
-                    comandsList.add(  buf_str.split("IF0")[1].split(",\"DO0\":")[1].split("\"ELSE\"")[0] );
-                    comandsList.add("ELSE");
-                    comandsList.add(  buf_str.split("IF0")[1].split(",\"DO0\":")[1].split("\"ELSE\"")[1] );
-                    comandsList.add("IF_END");
+                    comandsString_BUFFER.add(i,  buf_str.split("IF0")[1].split(",\"DO0\":")[0] );
+                    comandsString_BUFFER.add(i,  "DO");
+                    comandsString_BUFFER.add( i,   buf_str.split("IF0")[1].split(",\"DO0\":")[1].split("\"ELSE\"")[0] );
+                    comandsString_BUFFER.add(i,  "ELSE");
+                    comandsString_BUFFER.add( i,   buf_str.split("IF0")[1].split(",\"DO0\":")[1].split("\"ELSE\"")[1] );
+                    comandsString_BUFFER.add(i,  "IF_END");
                     thereAreAttachments = true;
                 }
                 else{
@@ -61,37 +70,37 @@ public class Main {
                         //System.out.println("iiiiiiiiiiiiiiiiii  "+i);
                         buf_str = "INIT_POSE " + comandsString[i].split("POSITION_NAME\":\"")[1].split("\"")[0].trim();
                         //System.out.println("\t\tPreparated code string: " + buf_str);
-                        comandsList.add(buf_str);
+                        comandsString_BUFFER.add(i,buf_str);
                     }
                     else {
                         if (comandsString[i].indexOf("moveTo") != -1){
                             buf_str = "PTP " + comandsString[i].split("POSITION_NAME\":\"")[1].split("\"")[0].trim();
                             //System.out.println("\t\tPreparated code string: " + buf_str);
-                            comandsList.add(buf_str);
+                            comandsString_BUFFER.add(i,  buf_str);
                         }
                         else {
                             if (comandsString[i].indexOf("debug_activate_notification") != -1){
                                 buf_str = "MESSAGE " + comandsString[i].split("\"TEXT\":\"")[1].split("\"")[0];
                                 //System.out.println("\t\tPreparated code string: " + buf_str);
-                                comandsList.add(buf_str);
+                                comandsString_BUFFER.add(i,  buf_str);
                             }
                             else {
                                 if (comandsString[i].indexOf("wait_seconds") != -1){
                                     buf_str = "WAIT_SEC " + comandsString[i].split("\"SECONDS\":")[1].split("\"")[0].split("}")[0];
                                     //System.out.println("\t\tPreparated code string: " + buf_str);
-                                    comandsList.add(buf_str);
+                                    comandsString_BUFFER.add(i,  buf_str);
                                 }
                                 else {
                                     if (comandsString[i].indexOf("getCurrentPosition") != -1){
                                         buf_str = "GET_CURRENT_POSE " + comandsString[i].split("\"AXIS\":")[1].split("\"")[1];
                                         //System.out.println("\t\tPreparated code string: " + buf_str);
-                                        comandsList.add(buf_str);
+                                        comandsString_BUFFER.add(i,  buf_str);
                                     }
                                     else {
                                         if (comandsString[i].indexOf("math_number") != -1){
                                             buf_str = "NUMBER " + comandsString[i].split("\"NUM\":")[1].split("\"")[0];
                                             //System.out.println("\t\tPreparated code string: " + buf_str);
-                                            comandsList.add(buf_str);
+                                            comandsString_BUFFER.add(i,  buf_str);
                                         }
                                         else {
 
@@ -105,23 +114,35 @@ public class Main {
                 System.out.println("iiiiiiiiiii " + i + " " + buf_str);
 
             }
-            System.out.println("START Print current massive");
-            for(int i = 0; i < comandsString.length; i++){
-                //System.out.println("\t" + comandsString[i]);
+            //System.out.println("START Print current massive");
+            for(int i = 0; i < comandsString_BUFFER.size(); i++){
+                if (comandsString_BUFFER.get(i) == "") {
+                    comandsString_BUFFER.remove(i);
+                }
+                //System.out.println("\t" + comandsString_BUFFER);
+            }
+            //System.out.println("END Print current massive");
+            System.out.println("START Print current massive" + comandsList.size());
+
+            for(int i = 0; i < comandsList.size(); i++){
+
+                System.out.println("\t" + comandsList.get(i));
             }
             System.out.println("END Print current massive");
+
 
             if(thereAreAttachments){
                 comandsList.add("");
                 comandsList.add("ITERATION");
                 comandsList.add("");
 
-                String[] dblArray = new String[comandsList.size()];
-                dblArray = comandsList.toArray(dblArray);
+                String[] dblArray = new String[comandsString_BUFFER.size()];
+                dblArray = comandsString_BUFFER.toArray(dblArray);
                 researchStringCode(dblArray);
                 thereAreAttachments = false;
             }
 
+            comandsList = comandsString_BUFFER;
 
         }
     }
